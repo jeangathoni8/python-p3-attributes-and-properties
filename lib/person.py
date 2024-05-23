b4b4a719d7e4b2b228151041225929504ffeda15
jeangathoni8 <jeangathoni39@gmail.com>
2024-05-23 12:22:13 +0300

    "Added properties and methods to Dog and Person classes, and defined approved breeds and jobs lists."

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,36 @@
-#!/usr/bin/env python3
+# lib/person.py
 
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
+approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
 
 class Person:
-    pass
+    def __init__(self, name="Person", job="Unemployed"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in approved_jobs:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+    
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
